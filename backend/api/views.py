53ffbfd4b98d275c92a61bcfcccb3b9e83f10082
William Jeffreys <wjeffreys96@gmail.com>
2023-02-15 13:02:15 -0800

    first commit

---
+++ backend/api/views.py
@@ -0,0 +1,15 @@
+from django.forms.models import model_to_dict
+from products.models import Product
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.serializers import ProductSerializer
+
+
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    instance = Product.objects.all().order_by("?").first()
+    data = {}
+    if instance:
+        # data = model_to_dict(instance, fields=["id", "title", "price", 'sale_price'])
+        data = ProductSerializer(instance).data
+    return Response(data)
